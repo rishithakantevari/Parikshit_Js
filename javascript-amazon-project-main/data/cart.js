975b5822d3b759e40de3e26be01c0be8367a39f7
rishithakantevari <rishithakantevari2207@gmail.com>
2025-06-27 16:32:17 +0530

    Set up your cart data

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,20 +1,44 @@
-export const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-export function addToCart(productId){
+if (!cart) {
+  cart = [];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
   let matchingItem;
 
-      cart.forEach((cartItem) => {
-        if (productId === item.productId) {
-          matchingItem = cartItem;
-        }
-      });
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
 
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
 }
